b5b4e45a668114a3476860a8de631a693092a399
Alexeev Alexander <work.alexeevs@gmail.com>
2023-04-27 10:10:34 +0300

    export type config

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export { ToastContainer } from './src/components/RootToast';
 export { toast } from './src/helpers/utils/eventBustToast';
+export type { ConfigToastType } from './src/interfaces/config';
 import './src/styles/styles.scss';
